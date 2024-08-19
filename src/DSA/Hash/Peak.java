ee553d3456e876feabc8f1c7e5f53fbccc498b13
Bundele-Durwangsingh <bundeledurwang@gmail.com>
2024-08-19 22:26:43 +0530

    Fix:Issue with peak program

--- src/DSA/Hash/Peak.java
+++ src/DSA/Hash/Peak.java
@@ -1,27 +1,35 @@
 package DSA.Hash;
+
 import java.util.HashMap;
 import java.util.Scanner;
+
 public class Peak {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         HashMap<Integer, Integer> map = new HashMap<>();
-        int [] arr= new int[n];
-        int max= 0;
-        for(int i=0;i<n;i++){
-            arr[i]=input.nextInt();
-            if(arr[i]>max){
-                max=arr[i];
+        int[] arr = new int[n];
+        int max = Integer.MIN_VALUE;
+
+        // Reading input and populating the map with frequencies
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+            if (arr[i] > max) {
+                max = arr[i];
             }
-            int count=map.containsKey(arr[i])?map.get(arr[i]):0;
-            map.put(arr[i],count+1);
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-        for(int key :map.keySet()){
-            if((key==max && map.get(key)>1)||map.get(key)>2){
+
+        // Checking conditions
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+            if ((key == max && freq > 1) || (key != max && freq > 2)) {
                 System.out.println("False");
-                System.exit(0);
+                input.close();
+                return;
             }
         }
+
         System.out.println("True");
         input.close();
     }
